d3ae893c774b7ae540a587aab278f3ff81808686
Khan Mohsin <m90khan@gmail.com>
2019-03-31 14:11:12 +0200

    App that fetches data from two sites and display on screen

---
+++ precache-manifest.036a160450f4418572909af19e9a4249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "71337c4611de1d4706a9",
+    "url": "/static/js/main.b7802deb.chunk.js"
+  },
+  {
+    "revision": "35dea5c3b1472b2a147d",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "71337c4611de1d4706a9",
+    "url": "/static/css/main.931658fb.chunk.css"
+  },
+  {
+    "revision": "35dea5c3b1472b2a147d",
+    "url": "/static/css/2.a7f7617f.chunk.css"
+  },
+  {
+    "revision": "fe43f67d079b600de8420db120e05de6",
+    "url": "/index.html"
+  }
+];
